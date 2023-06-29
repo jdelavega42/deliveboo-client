37f3180d74c9c00b079d3b905fb39a7b2fae682d
“jdelavega94” <jcdelavega94@gmail.com>
2023-06-29 17:48:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AppHome from './components/AppHome.vue'
+</script>
+
+<template>
+
+  <AppHome />
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/AppHome.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+<h1>Deliveboo</h1>
+</template>
+
+<style scoped>
+
+</style>
