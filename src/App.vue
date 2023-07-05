2817907b2536dd9d50907b0fef568f9afbbfb23f
francescovincidev <francescovinci2000@gmail.com>
2023-07-05 12:53:35 +0200

    add color on title and cookings, loading and messages of restaurants

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,5 @@ export default {
 
 <style lang="scss">
 @use "./style/general.scss" as *;
+@use "./style/partials/variables" as *;
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,6 +8,8 @@ export default {
             cookings: [],
             restaurants: [],
             cookingType: null,
+            loading: null,
+            message: null,
             store
         };
     },
@@ -22,8 +24,21 @@ export default {
             })
         },
         getRestaurants() {
-            axios.get(`${store.apiBaseUrl}/api/filtered_restaurant/` + this.cookingType).then((resp) => {
+            this.message = null;
+            this.loading = true;
+            this.restaurants = [];
+
+            axios.get(`${store.apiBaseUrl}/api/filtered_restaurant/` + this.cookingType.name).then((resp) => {
                 this.restaurants = resp.data.results;
+            }).finally(() => {
+                this.loading = false;
+                if (this.restaurants.length) {
+                    this.message = "Ecco i ristoranti per: " + this.cookingType.name
+                } else {
+                    this.message = "Nessun ristorante per: " + this.cookingType.name
+
+                }
+
             })
         }
 
@@ -33,12 +48,48 @@ export default {
 
 <template>
     <div class="container">
-        <h2>Cosa ti va oggi?</h2>
-        <div v-for="cooking in cookings" @click="cookingType = cooking.name, getRestaurants()">{{ cooking.name }}</div>
-        <div v-for="restaurant in restaurants">{{ restaurant.name }}</div>
-        <!-- <router-link :to="{ name: 'home' }">Visualizza i tuoi progetti</router-link> -->
+
+        <!-- COOKINGS TYPE -->
+        <h2 class="text-center">COSA TI VA OGGI?</h2>
+        <div class="cookings-list d-flex row row-cols-4">
+
+            <div class="col text-center" v-for="cooking in cookings">
+                <div class="cooking-type p-3 m-3 rounded" role='button' @click="cookingType = cooking, getRestaurants()">{{
+                    cooking.name }}</div>
+            </div>
+        </div>
+
+
+        <!-- RESTAURANTS LIST -->
+        <div class="restaurant-list" v-if="loading != null">
+
+            <h2 class="text-center">I nostri ristoranti</h2>
+
+            <div v-if="loading" class="text-center">CARICAMENTO</div>
+
+
+            <div>{{ message }}</div>
+            <div v-for="restaurant in restaurants">{{ restaurant.name }}</div>
+
+
+            <!-- <router-link :to="{ name: 'home' }">Visualizza i tuoi progetti</router-link> -->
+        </div>
 
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../style/general.scss" as *;
+@use "../style/partials/variables" as *;
+
+h2 {
+
+    color: $primary-green;
+
+}
+
+.cooking-type {
+    color: white;
+    background-color: $primary-red;
+}
+</style>
