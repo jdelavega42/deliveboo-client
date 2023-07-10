18b43aaebb5fe505536e9bed1a3adecf107319a7
Alessio Libbi <alex.libbi@gmail.com>
2023-07-10 16:35:28 +0200

    Add: button on cart for go back homepage // style on add to cart button

--- src/components/CartOffcanvas.vue
+++ src/components/CartOffcanvas.vue
@@ -1,131 +1,172 @@
 <script>
-import { store } from '../store';
-import { updateProducts, addToCart, removeFromCart, updateOrder, updateCart, productMultiplier, totalCart } from '../utilities/helpers';
+import { store } from "../store";
+import {
+  updateProducts,
+  addToCart,
+  removeFromCart,
+  updateOrder,
+  updateCart,
+  productMultiplier,
+  totalCart,
+} from "../utilities/helpers";
 export default {
-    name: "CartOffcanvas",
-    data() {
-        return {
-            store,
-            updateProducts,
-            addToCart,
-            removeFromCart,
-            updateOrder,
-            updateCart,
-            productMultiplier,
-            totalCart
-        }
+  name: "CartOffcanvas",
+  data() {
+    return {
+      store,
+      updateProducts,
+      addToCart,
+      removeFromCart,
+      updateOrder,
+      updateCart,
+      productMultiplier,
+      totalCart,
+    };
+  },
+  mounted() {
+    this.productMultiplier(), this.totalCart();
+    // this.store.state.total = this.store.state.cart.reduce((total, item) => total + parseFloat(item.price) * parseInt(item.quantity), 0);
+  },
+  methods: {
+    increment(product) {
+      product.quantity++;
+      this.store.updateLocalStorage();
     },
-    mounted() {
-        this.productMultiplier(),
-        this.totalCart()
-        // this.store.state.total = this.store.state.cart.reduce((total, item) => total + parseFloat(item.price) * parseInt(item.quantity), 0);
+
+    decrement(product) {
+      let currentValue = product.quantity;
+      if (currentValue > 1) {
+        product.quantity--;
+        this.store.updateLocalStorage();
+      }
     },
-    methods: {
+    updateQuantity(product, quantity) {
+      const newQuantity = parseInt(quantity.target.value);
 
-        increment(product) {
-            product.quantity++
-            this.store.updateLocalStorage();
-        },
-
-        decrement(product) {
-            let currentValue = product.quantity;
-            if (currentValue > 1) {
-                product.quantity--
-                this.store.updateLocalStorage();
-            }
-        },
-        updateQuantity(product, quantity) {
-
-            const newQuantity = parseInt(quantity.target.value);
-
-            // Verifica se il nuovo valore dell'input è valido
-            if (newQuantity && newQuantity >= 0) {
-                product.quantity = newQuantity;
-            } else {
-                product.quantity = 0;
-
-            }
-            this.store.updateLocalStorage();
-
-
-        },
-        deleteCartProduct(product) {
-            const index = this.store.state.cart.indexOf(product);
-            this.store.state.cart.splice(index, 1);
-            this.store.updateLocalStorage();
-
-
-        }
+      // Verifica se il nuovo valore dell'input è valido
+      if (newQuantity && newQuantity >= 0) {
+        product.quantity = newQuantity;
+      } else {
+        product.quantity = 0;
+      }
+      this.store.updateLocalStorage();
     },
-    computed: {
-        totalCost() {
-            let cost = this.store.state.cart.reduce((total, item) => total + parseFloat(item.price) * parseInt(item.quantity), 0);
-            this.store.state.total = cost
-            return cost
-        }
+    deleteCartProduct(product) {
+      const index = this.store.state.cart.indexOf(product);
+      this.store.state.cart.splice(index, 1);
+      this.store.updateLocalStorage();
     },
-}
+  },
+  computed: {
+    totalCost() {
+      let cost = this.store.state.cart.reduce(
+        (total, item) =>
+          total + parseFloat(item.price) * parseInt(item.quantity),
+        0
+      );
+      this.store.state.total = cost;
+      return cost;
+    },
+  },
+};
 </script>
 
 <template>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-        <div class="offcanvas-header">
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            <h3 class=" offcanvas-title fw-normal mb-0 text-black">Shopping Cart</h3>
-        </div>
-        <!-- lorem -->
-        <div class="offcanvas-body">
-            <div class="container py-1">
-                <div class="row d-flex justify-content-center align-items-center">
-                    <!-- INIZIO SINGOLA CARTA -->
-                    <div class="card rounded-3 mb-2" v-for="product in store.state.cart">
-                        <div class="row d-flex justify-content-between align-items-center">
-                            <div class="d-flex justify-content-between p-1">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                                    class="img-fluid rounded-3 col-6" alt="Cotton T-shirt">
-                                <div class="col-6 text-center p-1 d-flex flex-column justify-content-between">
-                                    <p class="lead fw-normal mb-2">{{ product.name }}</p>
-
-                                    <!-- aggiungere funzione productMultiplier -->
-                                    <h5 class="mb-0">&euro;<span class="price">{{ product.price }}</span></h5>
-
-                                    <div class="d-flex">
-                                        <div class="p-2" @click="decrement(product)">
-                                            <i class="fa-solid fa-minus"></i>
-                                        </div>
-
-
-                                        <!-- Modificare value -->
-                                        <input id="form1" min="1" name="quantity" type="number"
-                                            class="text-center form-control form-control-sm" :value="product.quantity"
-                                            @input="updateQuantity(product, $event)" />
-
-                                        <div class="p-2" @click="increment(product)">
-                                            <i class="fa-solid fa-plus"></i>
-                                        </div>
-                                    </div>
-                                    <div class="btn  w-100 btn-outline-danger" @click="deleteCartProduct(product)"><i
-                                            class="fas fa-trash"></i></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- FINE SINGOLA CARTA -->
-
-                    <div class="card p-1 mt-4">
-                        <div class="d-flex justify-content-around align-items-center">
-                            <p class="lead fw-normal mb-2">Your Order</p>
-
-                            <!-- aggiungere funzione productMultiplier -->
-                            <h5>&euro;<span id="totalCartPrice">{{ totalCost }}</span></h5>
-                        </div>
-                        <router-link :to="{name: 'order'}" class="btn btn-success btn-block btn-lg">Checkout</router-link>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div
+    class="offcanvas offcanvas-end"
+    tabindex="-1"
+    id="offcanvasExample"
+    aria-labelledby="offcanvasExampleLabel"
+  >
+    <div class="offcanvas-header">
+      <button
+        type="button"
+        class="btn-close text-reset"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+      <h3 class="offcanvas-title fw-normal mb-0 text-black">Shopping Cart</h3>
     </div>
+    <!-- lorem -->
+    <div class="offcanvas-body">
+      <div class="container py-1">
+        <div class="row d-flex justify-content-center align-items-center">
+          <!-- INIZIO SINGOLA CARTA -->
+          <div class="card rounded-3 mb-2" v-for="product in store.state.cart">
+            <div class="row d-flex justify-content-between align-items-center">
+              <div class="d-flex justify-content-between p-1">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
+                  class="img-fluid rounded-3 col-6"
+                  alt="Cotton T-shirt"
+                />
+                <div
+                  class="col-6 text-center p-1 d-flex flex-column justify-content-between"
+                >
+                  <p class="lead fw-normal mb-2">{{ product.name }}</p>
+
+                  <!-- aggiungere funzione productMultiplier -->
+                  <h5 class="mb-0">
+                    &euro;<span class="price">{{ product.price }}</span>
+                  </h5>
+
+                  <div class="d-flex">
+                    <div class="p-2" @click="decrement(product)">
+                      <i class="fa-solid fa-minus"></i>
+                    </div>
+
+                    <!-- Modificare value -->
+                    <input
+                      id="form1"
+                      min="1"
+                      name="quantity"
+                      type="number"
+                      class="text-center form-control form-control-sm"
+                      :value="product.quantity"
+                      @input="updateQuantity(product, $event)"
+                    />
+
+                    <div class="p-2" @click="increment(product)">
+                      <i class="fa-solid fa-plus"></i>
+                    </div>
+                  </div>
+                  <div
+                    class="btn w-100 btn-outline-danger"
+                    @click="deleteCartProduct(product)"
+                  >
+                    <i class="fas fa-trash"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- FINE SINGOLA CARTA -->
+
+          <div class="card p-1 mt-4">
+            <div class="d-flex justify-content-around align-items-center">
+              <p class="lead fw-normal mb-2">Your Order</p>
+
+              <!-- aggiungere funzione productMultiplier -->
+              <h5>
+                &euro;<span id="totalCartPrice">{{ totalCost }}</span>
+              </h5>
+            </div>
+            <router-link
+              :to="{ name: 'order' }"
+              class="btn btn-success btn-block btn-lg"
+              >Checkout</router-link
+            >
+            <router-link
+              :to="{ name: 'home' }"
+              class="btn btn-success btn-block btn-lg"
+              >Continua con gli acquisti</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
--- src/pages/RestaurantMenu.vue
+++ src/pages/RestaurantMenu.vue
@@ -2,166 +2,190 @@
 import axios from "axios";
 import { store } from "../store";
 export default {
-    name: "RestaurantMenu",
-    data() {
-        return {
-            restaurant: [],
-            errorMessage: "",
-            products: [],
-            quantity: [],
-            // errorQuantity: "",
-            store,
-        };
+  name: "RestaurantMenu",
+  data() {
+    return {
+      restaurant: [],
+      errorMessage: "",
+      products: [],
+      quantity: [],
+      // errorQuantity: "",
+      store,
+    };
+  },
+  mounted() {
+    const slug = this.$route.params.slug;
+    axios.get(`${store.apiBaseUrl}/api/restaurantMenu/${slug}`).then((resp) => {
+      if (resp.data.success) {
+        this.restaurant = resp.data.results[0];
+        this.products = this.restaurant.product;
+        this.quantity = this.products.map(() => 1); // Inizializza l'array dei valori con 1 per ogni input
+      } else {
+        this.errorMessage = `${slug} non è un ristorante`;
+      }
+    });
+  },
+  methods: {
+    increment(index) {
+      this.quantity[index]++;
     },
-    mounted() {
-        const slug = this.$route.params.slug;
-        axios.get(`${store.apiBaseUrl}/api/restaurantMenu/${slug}`).then((resp) => {
-            if (resp.data.success) {
-                this.restaurant = resp.data.results[0];
-                this.products = this.restaurant.product;
-                this.quantity = this.products.map(() => 1); // Inizializza l'array dei valori con 1 per ogni input
 
-
-            } else {
-                this.errorMessage = `${slug} non è un ristorante`;
-            }
+    decrement(index) {
+      if (this.quantity[index] > 1) {
+        this.quantity[index]--;
+      }
+    },
+    addQuantity(index, quantity) {
+      if (quantity > 1) {
+        this.quantity[index] = parseInt(quantity);
+        // this.errorQuantity = ""
+      } else {
+        // this.errorQuantity = "La quantità deve essere maggiore di 0"
+      }
+    },
+    addToCart(product, index) {
+      const existingProduct = this.store.state.cart.find(
+        (item) => item.id === product.id
+      );
+      if (existingProduct && this.quantity[index] > 0) {
+        existingProduct.quantity = this.quantity[index];
+        this.store.updateLocalStorage();
+      } else if (this.quantity[index] > 0) {
+        // Il prodotto non esiste nell'array "cart"
+        // Aggiungi il prodotto all'array "cart"
+        this.store.state.cart.push({
+          ...product,
+          quantity: this.quantity[index],
         });
-
+        this.store.updateLocalStorage();
+      }
     },
-    methods: {
-        increment(index) {
-            this.quantity[index]++;
-        },
-
-        decrement(index) {
-
-            if (this.quantity[index] > 1) {
-                this.quantity[index]--;
-            }
-        },
-        addQuantity(index, quantity) {
-            if (quantity > 1) {
-                this.quantity[index] = parseInt(quantity);
-                // this.errorQuantity = ""
-
-            } else {
-                // this.errorQuantity = "La quantità deve essere maggiore di 0"
-
-            }
-        },
-        addToCart(product, index) {
-
-            const existingProduct = this.store.state.cart.find((item) => item.id === product.id);
-            if (existingProduct && this.quantity[index] > 0) {
-                existingProduct.quantity = this.quantity[index];
-                this.store.updateLocalStorage();
-
-            } else if (this.quantity[index] > 0) {
-                // Il prodotto non esiste nell'array "cart"
-                // Aggiungi il prodotto all'array "cart"
-                this.store.state.cart.push({ ...product, quantity: this.quantity[index] });
-                this.store.updateLocalStorage();
-            }
-        }
-
-    },
+  },
 };
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Logo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container-fluid">
-        <!-- RESTAURANT INFO -->
-        <div class="row row-cols-2">
-            <!-- NAME AND MORE INFO -->
-            <div class="col restaurant-info restaurant-name">
-                <div class="info d-flex flex-column justify-content-center align-items-center h-100">
-                    <h1 class="name m-1">{{ restaurant.name }}</h1>
-                    <div v-for="cooking in restaurant.cookings" class="cookings m-3">
-                        {{ cooking.name }}
-                    </div>
-                    <div class="description m-3">
-                        {{ restaurant.PIVA }}
-                    </div>
-                    <div class="address m-2">{{ restaurant.address }}</div>
-                    <div class="number m-1">{{ restaurant.number }}</div>
-                </div>
-            </div>
-
-            <!-- IMAGES -->
-            <div class="col restaurant-info restaurant-image">
-                <div class="image d-flex justify-content-center align-items-center h-100">
-                    <h2>IMMAGINI</h2>
-                </div>
-            </div>
-        </div>
-        <!-- MENU -->
-        <h2 class="text-center mt-3">I nostri prodotti</h2>
-        <div class="menu d-flex flex-column align-items-center">
-            <div class="single-product rounded m-2 d-flex justify-content-around align-items-center"
-                v-for="(product, index) in products" :key="index">
-                <div class="images">
-                    <img class="rounded" src="../assets/img/pizza_ph.jpg" alt="" />
-                </div>
-                <div class="info d-flex flex-column h-100 justify-content-around">
-                    <div class="name mt-2">{{ product.name }}</div>
-                    <div class="description">{{ product.description }}</div>
-                    <div class="price my-2 me-3 align-self-end">
-                        Prezzo {{ product.price }} $
-                    </div>
-                    <div v-if="product.visibility">Il prodotto è disponibile</div>
-                    <div v-else>Il Prodotto non è disponibile</div>
-                </div>
-                <div class="cart-section d-flex flex-column h-100 justify-content-around h-100">
-                    <div class="text-center" for="number">Quantità:</div>
-                    <div class="position-relative quantity-form">
-                        <div class="position-absolute error-quantity" v-if="quantity[index] < 1">"Inserisci un valore
-                            maggiore di 0"</div>
-
-                        <div class="d-flex align-items-center h-100">
-                            <div class="p-2" @click="decrement(index)">
-                                <i class="fa-solid fa-minus"></i>
-                            </div>
-                            <input type="number" :id="'number-' + product.id" class="quantity-number form-control"
-                                name="number" v-model="quantity[index]" @input="addQuantity(index, $event.target.value)"
-                                min="1" />
-
-                            <div class="p-2" @click="increment(index)">
-                                <i class="fa-solid fa-plus"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="text-center" @click="addToCart(product, index)">
-                        Aggiungi al carrello <i class="fa-solid fa-cart-plus"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+  <div class="container-fluid">
+    <!-- RESTAURANT INFO -->
+    <div class="row row-cols-2">
+      <!-- NAME AND MORE INFO -->
+      <div class="col restaurant-info restaurant-name">
+        <div
+          class="info d-flex flex-column justify-content-center align-items-center h-100"
+        >
+          <h1 class="name m-1">{{ restaurant.name }}</h1>
+          <div v-for="cooking in restaurant.cookings" class="cookings m-3">
+            {{ cooking.name }}
+          </div>
+          <div class="description m-3">
+            {{ restaurant.PIVA }}
+          </div>
+          <div class="address m-2">{{ restaurant.address }}</div>
+          <div class="number m-1">{{ restaurant.number }}</div>
+        </div>
+      </div>
+
+      <!-- IMAGES -->
+      <div class="col restaurant-info restaurant-image">
+        <div
+          class="image d-flex justify-content-center align-items-center h-100"
+        >
+          <h2>IMMAGINI</h2>
+        </div>
+      </div>
+    </div>
+    <!-- MENU -->
+    <h2 class="text-center mt-3">I nostri prodotti</h2>
+    <div class="menu d-flex flex-column align-items-center">
+      <div
+        class="single-product rounded m-2 d-flex justify-content-around align-items-center"
+        v-for="(product, index) in products"
+        :key="index"
+      >
+        <div class="images">
+          <img class="rounded" src="../assets/img/pizza_ph.jpg" alt="" />
+        </div>
+        <div class="info d-flex flex-column h-100 justify-content-around">
+          <div class="name mt-2">{{ product.name }}</div>
+          <div class="description">{{ product.description }}</div>
+          <div class="price my-2 me-3 align-self-end">
+            Prezzo {{ product.price }} $
+          </div>
+          <div v-if="product.visibility">Il prodotto è disponibile</div>
+          <div v-else>Il Prodotto non è disponibile</div>
+        </div>
+        <div
+          class="cart-section d-flex flex-column h-100 justify-content-around h-100"
+        >
+          <div class="text-center" for="number">Quantità:</div>
+          <div class="position-relative quantity-form">
+            <div
+              class="position-absolute error-quantity"
+              v-if="quantity[index] < 1"
+            >
+              "Inserisci un valore maggiore di 0"
+            </div>
+
+            <div class="d-flex align-items-center h-100">
+              <div class="p-2" @click="decrement(index)">
+                <i class="fa-solid fa-minus"></i>
+              </div>
+              <input
+                type="number"
+                :id="'number-' + product.id"
+                class="quantity-number form-control"
+                name="number"
+                v-model="quantity[index]"
+                @input="addQuantity(index, $event.target.value)"
+                min="1"
+              />
+
+              <div class="p-2" @click="increment(index)">
+                <i class="fa-solid fa-plus"></i>
+              </div>
+            </div>
+          </div>
+          <div class="text-center" @click="addToCart(product, index)">
+            <span class="add-cart"
+              >Aggiungi al carrello <i class="fa-solid fa-cart-plus"></i
+            ></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -169,67 +193,85 @@ export default {
 @use "../style/partials/variables" as *;
 
 .restaurant-info {
-    height: 500px;
+  height: 500px;
 }
 
 .restaurant-name {
-    background-color: $primary-green;
-    color: white;
-    font-size: 1.2rem;
+  background-color: $primary-green;
+  color: white;
+  font-size: 1.2rem;
 
-    .cookings {
-        font-size: 1.5rem;
-    }
+  .cookings {
+    font-size: 1.5rem;
+  }
 
-    .description {
-        width: 90%;
-    }
+  .description {
+    width: 90%;
+  }
 }
 
 h2 {
-    color: $primary-green;
-    font-weight: bold;
+  color: $primary-green;
+  font-weight: bold;
 }
 
 .single-product {
-    height: 140px;
-    border: 2px solid $primary-red;
-    width: 85%;
+  height: 140px;
+  border: 2px solid $primary-red;
+  width: 85%;
 
-    .images {
-        height: 90%;
+  .images {
+    height: 90%;
 
-        img {
-            height: 100%;
-            max-width: 170px;
-        }
+    img {
+      height: 100%;
+      max-width: 170px;
     }
+  }
 
-    .info {
-        width: 65%;
+  .info {
+    width: 65%;
 
-        .name {
-            font-size: 1.2rem;
-        }
+    .name {
+      font-size: 1.2rem;
     }
+  }
 
-    .cart-section {
-        height: 90%;
-        width: 15%;
+  .cart-section {
+    height: 90%;
+    width: 15%;
 
-        .error-quantity {
-            color: red;
-            top: -20px;
-            font-size: .8rem;
-        }
+    .error-quantity {
+      color: red;
+      top: -20px;
+      font-size: 0.8rem;
     }
+  }
+}
+.add-cart {
+  display: inline-block;
+  border-radius: 5px;
+  padding: 5px 15px;
+  cursor: pointer;
+  transition: transform 0.3s, box-shadow 0.3s, background-color 2s, color 1s;
+  background-color: white;
+  color: black;
+  &:hover {
+    transform: translateX(10px) scale(1.1);
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 1), -5px 0 10px rgba(0, 0, 0, 1);
+    background-color: $primary-green;
+    color: white;
+  }
+}
+i {
+  margin-left: 5px;
 }
 
 // PER NON MOSTRARE LA BARRA CHE INCREMENTA I NUMERI
 input[type="number"]::-webkit-inner-spin-button,
 input[type="number"]::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-    display: none;
+  -webkit-appearance: none;
+  margin: 0;
+  display: none;
 }
 </style>
