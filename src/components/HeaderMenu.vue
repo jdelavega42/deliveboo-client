711a1a6a85de599f5361666fe27f788a3c1e5e33
“jdelavega94” <jcdelavega94@gmail.com>
2023-07-16 01:40:12 +0200

    header restyle completed

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -27,24 +27,25 @@ export default {
             <span v-if="store.state.cart.length > 0" class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto text-end">
+            <ul class="navbar-nav ml-auto align-items-end">
                 <li class="nav-item">
                     <router-link
                       :to="{ name: 'home'}"
-                      role="button">
-                      <a class="nav-link text-decoration-none" href="#">Home</a>
+                      role="button"
+                      class="text-decoration-none">
+                      <a class="nav-link position-relative py-0 text-success fw-bold" href="#">Home</a>
                     </router-link>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000">Sei un ristoratore?</a>
+                <li class="nav-item ">
+                    <a class="nav-link position-relative py-0 text-success fw-bold" href="http://localhost:8000">Sei un ristoratore?</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Contatti</a>
+                    <a class="nav-link position-relative py-0 text-success fw-bold" href="#">Contatti</a>
                 </li>
                 <li class="nav-item">
-                    <button class="btn btn-link position-relative" type="button" data-bs-toggle="offcanvas"
+                    <button class="btn border-0 position-relative" type="button" data-bs-toggle="offcanvas"
                         data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-                        <i class="fa-solid fa-cart-shopping fa-2x"></i>
+                        <i class="text-success fa-solid fa-cart-shopping fa-2x" id="cart"></i>
                         <span v-if="store.state.cart.length > 0" class="position-absolute badge rounded-pill bg-danger"> {{ store.state.cart.length }} </span>
                     </button>
                 </li>
@@ -59,42 +60,30 @@ export default {
 @use "../style/general.scss" as *;
 @use "../style/partials/variables" as *;
 
-.header-menu {
-    height: 80px;
-    box-shadow: 0 4px 2px -2px gray;
-
-    .smaller-image {
-    max-width: 50%;
-    height: auto;
+a::after{
+    content: "";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: $primary-red;
+  transform: scaleX(0);
+  transition: transform 200ms ease;
 }
-
-    ul {
-        list-style-type: none;
-        height: 100%;
-        margin: 0;
-
-        a {
-            color: $primary-green;
-            text-decoration: none;
-            font-size: 1.2rem;
-        }
-    }
+.nav-item:hover a.nav-link::after {
+    transform: scale(1);
 }
-
-@media (max-width: 768px) {
-    .header-menu {
-        padding: 10px;
-    }
-    .header-menu img {
-        width: 50%;
-    }
-    .header-menu ul {
-        flex-direction: column;
-        align-items: center;
-        text-align: center;
-    }
-    .header-menu ul li {
-        margin-bottom: 10px;
-    }
+#cart:hover {
+    animation-name: floating;
+    animation-duration: 2000ms;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+}
+@keyframes floating {
+    0% { transform: translate(0,  0px); }
+    50%  { transform: translate(0, 5px); }
+    100%   { transform: translate(0, 0px); }   
 }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -66,52 +66,54 @@ export default {
 
 <template>
   <div class="container">
-    
+
     <!-- COOKINGS TYPE -->
-    <div class="">
-      <h2 class="fs-1 fw-bolder border-bottom text-success text-center m-5 text-uppercase">Cosa ti va oggi?</h2>
+    <div>
+      <h2 class="fs-1 fw-bolder border-bottom border-danger text-success text-center my-5 text-uppercase">Cosa ti va oggi?</h2>
       <div class="row">
-        <div class="col-sm-4 col-md-3 col-lg-2 text-center p-3" v-for="cooking in cookings">
-          <span class="btn btn-outline-danger border-3 rounded text-capitalize fw-bold w-100 py-3 shadow" :class="{ 'btn-danger text-white': params.includes(cooking.name) }" role="button"
-          @click="getParams(cooking.name), getRestaurants()">
-          {{ cooking.name }}
-        </span>
+        <div class="col-sm-6 col-md-4 col-lg-2 text-center p-3" v-for="cooking in cookings">
+          <span class="btn btn-outline-danger border-3 rounded text-capitalize fw-bold w-100 py-3 shadow"
+            :class="{ 'btn-danger text-white': params.includes(cooking.name) }" role="button"
+            @click="getParams(cooking.name), getRestaurants()">
+            {{ cooking.name }}
+          </span>
+        </div>
       </div>
     </div>
-  </div>
-    
+
     <!-- RESTAURANTS LIST -->
     <div v-if="loading != null">
-      <h2 class="fs-1 fw-bolder border-bottom text-success text-center m-5">I nostri ristoranti</h2>
-      <div v-if="loading" class="text-center">CARICAMENTO</div>
+      <h2 class="fs-1 fw-bolder border-bottom border-danger text-success text-center text-uppercase my-5">I nostri ristoranti</h2>
+      <div v-if="loading" class="mt-5 fs-3 fw-bolder text-center text-uppercase">caricamento</div>
 
-      <h2 class="text-center">{{ message }}</h2>
+      <h2 class="fs-3">{{ message }}</h2>
       <div class="row justify-content-center">
 
         <template v-for="restaurant in restaurants">
-          <div  class="col-lg-4 col-md-6 col-sm-12 p-3">
-            <router-link :to="{ name: 'restaurant', params: { slug: restaurant.slug } }"
-            role="button"  class="ms-hover card text-center p-0 text-decoration-none shadow-lg border border-success border-2">
-            
-            <p class="card-header fs-3 bg-success text-white">{{ restaurant.name }}</p>
-            <ul class="list-group border border-0">
-              <li class="list-group-item fs-5">
-                <span v-for="cooking in restaurant.cookings" class="badge rounded-pils text-bg-danger m-1">{{ cooking.name }}</span>
-              </li>
-              <li class="list-group-item">
-                {{ restaurant.address }}
-              </li>
-              <li class="list-group-item">
-                {{ restaurant.number }}
-              </li>
-              <li class="list-group-item">
-                {{ restaurant.email }}
-              </li>
-            </ul>
-          </router-link>
-        </div>
-      </template>
-    </div>
+          <div class="col-lg-4 col-md-6 col-sm-12 p-3">
+            <router-link :to="{ name: 'restaurant', params: { slug: restaurant.slug } }" role="button"
+              class="ms-hover card text-center p-0 text-decoration-none shadow-lg border border-success border-2">
+
+              <p class="card-header fs-3 fw-bolder bg-success text-white">{{ restaurant.name }}</p>
+              <ul class="list-group border border-0">
+                <li class="list-group-item fs-5">
+                  <span v-for="cooking in restaurant.cookings" class="badge rounded-pils text-bg-danger m-1">{{
+                    cooking.name }}</span>
+                </li>
+                <li class="list-group-item">
+                  {{ restaurant.address }}
+                </li>
+                <li class="list-group-item">
+                  {{ restaurant.number }}
+                </li>
+                <li class="list-group-item">
+                  {{ restaurant.email }}
+                </li>
+              </ul>
+            </router-link>
+          </div>
+        </template>
+      </div>
 
     </div>
   </div>
@@ -124,7 +126,7 @@ export default {
 .ms-hover {
   transition: 200ms;
 }
+
 .ms-hover:hover {
   transform: scale(1.05);
-}
-</style>
+}</style>
